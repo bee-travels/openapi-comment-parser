b4b2fb29a6201c78e994ac52f95c2f8dbb214173
Nick <bourdakos1@gmail.com>
2020-06-29 12:46:32 -0400

    update cli

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
+const path = require('path');
 const fs = require('fs');
 const parseComments = require('./../dist/openapi-comment-parser');
 
-const outputPath = process.argv[2];
+if (process.argv[2] === undefined || process.argv[3] === undefined) {
+  console.log('Usage:');
+  console.log('  openapi-comment-parser SRC_PATH OUTPUT');
+  console.log();
+  console.log('Example:');
+  console.log('  openapi-comment-parser . openapi.json');
+  return;
+}
 
-const spec = parseComments({ cwd: process.cwd() });
+const inputPath = path.resolve(process.argv[2]);
+const outputPath = path.resolve(process.argv[3]);
+
+const spec = parseComments({ cwd: inputPath });
 
 fs.writeFileSync(outputPath, JSON.stringify(spec));
